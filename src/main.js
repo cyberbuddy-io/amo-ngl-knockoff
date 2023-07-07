331538b53d00319623d5e8c7870d61aa79df72bd
Arshpreet <avibedi1234@gmail.com>
2023-07-08 02:06:54 +0530

    css

---
+++ src/main.js
@@ -0,0 +1,29 @@
+function togglePasswordVisibility() {
+    let passwordInput = document.getElementById("password");
+    let toggleIcon = document.getElementById("toggleIcon");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        toggleIcon.classList.remove("fa-eye");
+        toggleIcon.classList.add("fa-eye-slash");
+    } else {
+        passwordInput.type = "password";
+        toggleIcon.classList.remove("fa-eye-slash");
+        toggleIcon.classList.add("fa-eye");
+    }
+}
+
+function retogglePasswordVisibility() {
+    let passwordInput = document.getElementById("repassword");
+    let toggleIcon = document.getElementById("retoggleIcon");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        toggleIcon.classList.remove("fa-eye");
+        toggleIcon.classList.add("fa-eye-slash");
+    } else {
+        passwordInput.type = "password";
+        toggleIcon.classList.remove("fa-eye-slash");
+        toggleIcon.classList.add("fa-eye");
+    }
+}
